1ebe7bd863fd4abc28604c852b97d83a175b8ec0
Mutasem Mustafa <itsmutasem@gmail.com>
2026-02-22 23:10:45 +0300

    functions

--- src/index.ts
+++ src/index.ts
@@ -6,35 +6,42 @@
 // console.log(name);
 // console.log(Role.ADMIN);
 
-// Core types & type inference
-let name = 'Mutasem';
-let age: number = 21;
-let isAdmin: boolean = true;
-let hobbies: string[] = ['Reading', 'Coding'];
-hobbies.push('Traveling');
-// objects
-let student: { name:string, age:number, isStudent:boolean} = {name: 'Mutasem', age: 21, isStudent: true};
-// array of objects
-let students: { name:string, age:number, isStudent:boolean}[];
-students = [
-    {name: 'Mutasem', age: 21, isStudent: true},
-    {name: 'Ali', age: 22, isStudent: false},
-    {name: 'Mohammad', age: 23, isStudent: true}
-];
-console.log(students);
-// null
-let user: { name:string, age?:number} | null = null;
-console.log("user before assignment", user);
-user = {name: 'Mutasem', age: 21};
-console.log("user after assignment", user);
-user = null;
-console.log("user after assignment to null", user);
-// json
-let json: { name:string, age:number} | unknown = JSON.parse('{"name": "Mutasem json", "age": "21"}');
-console.log(json);
-if(typeof json === 'object' && json !== null && "name" in json){
-    console.log("its object", json);
-    console.log(json.name);
-} else {
-    console.log('json is not an object', json);
+// // Core types & type inference
+// let name = 'Mutasem';
+// let age: number = 21;
+// let isAdmin: boolean = true;
+// let hobbies: string[] = ['Reading', 'Coding'];
+// hobbies.push('Traveling');
+// // objects
+// let student: { name:string, age:number, isStudent:boolean} = {name: 'Mutasem', age: 21, isStudent: true};
+// // array of objects
+// let students: { name:string, age:number, isStudent:boolean}[];
+// students = [
+//     {name: 'Mutasem', age: 21, isStudent: true},
+//     {name: 'Ali', age: 22, isStudent: false},
+//     {name: 'Mohammad', age: 23, isStudent: true}
+// ];
+// console.log(students);
+// // null
+// let user: { name:string, age?:number} | null = null;
+// console.log("user before assignment", user);
+// user = {name: 'Mutasem', age: 21};
+// console.log("user after assignment", user);
+// user = null;
+// console.log("user after assignment to null", user);
+// // json
+// let json: { name:string, age:number} | unknown = JSON.parse('{"name": "Mutasem json", "age": "21"}');
+// console.log(json);
+// if(typeof json === 'object' && json !== null && "name" in json){
+//     console.log("its object", json);
+//     console.log(json.name);
+// } else {
+//     console.log('json is not an object', json);
+// }
+
+// functions
+function add(a: number, b: number): number {
+    return a + b;
 }
+const result = add(1, 2);
+console.log(result);
